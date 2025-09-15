fe9bc172a0d07405029b09497949eafcd44ec3ac
siyabonga <siyabongakhanyile76@gmail.com>
2025-09-15 11:55:44 +0200

    mod:added routes to the app for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import UserDetails from "./pages/UserDetails";
 
 function App() {
-
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/userdetails" element={<UserDetails />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
